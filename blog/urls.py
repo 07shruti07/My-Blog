b88cf694646a7faf387cc1aefda7597f272034d3
Shruti <shruti.k@amazatic.com>
2019-01-24 11:52:05 +0530

    Added views to create/edit blog inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
 	path('', views.post_list,name='post_list'),	
     path('post/<int:pik>/', views.post_detail, name='post_detail'),
+    path('post/new',views.post_new, name='post_new'),
+    path('post/<int:pku>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pkd>/delete',views.post_delete, name='post_delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+
 # Create your views here.
 
 def post_list(request):
@@ -11,3 +14,37 @@ def post_list(request):
 def post_detail(request,pik):
 	post=get_object_or_404(Post, pk=pik)
 	return render(request, 'blog/post_detail.html',{'post':post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pik=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request,pku):
+	post = get_object_or_404(Post,pk=pku)
+	if request.method=="POST":
+		form=PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post= form.save(commit=False)
+			post.author=request.user
+			post.published_date=timezone.now()
+			post.save()
+			return redirect('post_detail', pik=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request,'blog/post_edit.html',{'form':form, 'post':post})
+
+def post_delete(request,pkd):
+	post=get_object_or_404(Post,pk=pkd)
+	if request.method=="GET":
+		post.delete()
+	return redirect('post_list')
